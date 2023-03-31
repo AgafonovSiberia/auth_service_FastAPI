cbcc5a8cfdb4d6cea6b0aa50c92034592654408f
AgafonovSiberia <agafonov@chita.ru>
2023-03-31 16:16:41 +0700

    fixed celery init

--- src/app/config_reader.py
+++ src/app/config_reader.py
@@ -18,8 +18,8 @@ class Settings(BaseSettings):
     API_V1_URL: str = "/api/v1"
 
     POSTGRES_USER: str = Field(default="admin")
-    POSTGRES_PASSWORD: str = Field(default="qwerty123")
-    POSTGRES_DB: str = Field(default="auth_service")
+    POSTGRES_PASSWORD: str = Field(default="civiclaeu3")
+    POSTGRES_DB: str = Field(default="service")
     POSTGRES_HOST: str = Field(default="db")
     POSTGRES_PORT: str = Field(default="5432")
 
