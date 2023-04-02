9a51eb600e8da0176a80a48ef04c853e6ef39b72
AgafonovSiberia <agafonov@chita.ru>
2023-04-02 14:06:56 +0700

    efwf

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-from .config_reader import config
+
 from app.api.setup import setup
 from app.infrastructure.db.factory import create_pool
+
+from .config_reader import config
 
 
 def crate_app():
     return FastAPI(
         title="AuthService",
-        description="authorization service",
+        description="authorization service_2",
         openapi_url=f"{config.API_V1_URL}/openapi.json",
     )
 
